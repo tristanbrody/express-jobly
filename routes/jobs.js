ef84318f464ada6293a40f4f148abaa8ed1c6207
Tristan <tristandavisbrody@gmail.com>
2021-06-14 22:34:05 -0700

    -

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,7 +6,7 @@ const Job = require('../models/job');
 const jobNewSchema = require('../schemas/jobNew.json');
 const jobUpdateSchema = require('../schemas/jobUpdate.json');
 
-const router = new express.router();
+const router = new express.Router();
 
 //get all jobs in database (optionally can pass in filters in query string)
 router.get('/q/search', async function (req, res, next) {
@@ -22,6 +22,43 @@ router.get('/:id', async function (req, res, next) {
 	try {
 		const job = await Job.get(id);
 		return res.json({ job });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.post('/', async function (req, res, next) {
+	try {
+		const validator = jsonschema.validate(req.body, jobNewSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map(e => e.stack);
+			throw new BadRequestError(errs);
+		}
+		const job = await Job.create(req.body);
+		return res.status(201).json({ job });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.patch('/:id', async function (req, res, next) {
+	try {
+		const validator = jsonschema.validate(req.body, jobUpdateSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map(e => e.stack);
+			throw new BadRequestError(errs);
+		}
+		const job = await Job.update(req.params.id, req.body);
+		return res.json({ job });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.delete('/id', async function (req, res, next) {
+	try {
+		await Job.remove(req.params.id);
+		return res.json({ deleted: req.params.id });
 	} catch (err) {
 		return next(err);
 	}
--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -1,45 +1,28 @@
 const request = require('supertest');
-const db = require('../db');
+jest.mock('../db');
 const app = require('../app');
-require('dotenv').config();
-require('pg');
-const express = require('express');
 
-const { commonBeforeAll, commonBeforeEach, commonAfterEach, commonAfterAll, u4Token } = require('./_testCommon');
+// const { commonBeforeAll, commonBeforeEach, commonAfterEach, commonAfterAll, u4Token } = require('./_testCommon');
 
-beforeAll(commonBeforeAll);
-beforeEach(commonBeforeEach);
-afterEach(commonAfterEach);
-afterAll(commonAfterAll);
+// beforeAll(commonBeforeAll);
+// beforeEach(commonBeforeEach);
+// afterEach(commonAfterEach);
+// afterAll(commonAfterAll);
 
 /* attempt to write these tests using a mock client so no actual interaction with DB is required */
-jest.mock('pg', () => {
-	const { getDatabaseUri } = require('../config.js');
-	const testURI = getDatabaseUri();
-	const mClient = {
-		connectionString: testURI
-	};
-	return { Client: jest.fn(() => mClient) };
-});
-
-jest.mock('express', () => ({
-	Router: () => jest.fn()
-}));
 
 describe('get routes', function () {
-	beforeEach(() => {
-		let db = new Client();
-	});
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
 	test('get route with id should use correct SQL query', async function () {
-		router
-			.get(1)
-			.mockResolvedValueOnce({ title: 'someTitle', salary: 1000, equity: 0, company_handle: 'something' });
-		expect(client.query).toHaveBeenCalledWith(
-			`SELECT title, salary, equity, company_handle FROM jobs WHERE id = $1`,
-			[1]
-		);
+		let db = require('../db');
+		db.query.mockResolvedValueOnce({
+			rows: [{ title: 'someTitle', salary: 1000, equity: 0, company_handle: 'something' }]
+		});
+		request(app).get('/jobs/1');
+		expect(db.query).toHaveBeenCalledWith(`SELECT title, salary, equity, company_handle FROM jobs WHERE id = $1`, [
+			1
+		]);
 	});
 });
